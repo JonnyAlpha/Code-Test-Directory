2fe1492a78d3395c75ec326c366aa534cfca898e
JonnyAlpha <billharvs@btinternet.com>
2023-12-03 13:34:26 +0000

    Second commit

--- face_recognition.py
+++ face_recognition.py
@@ -11,8 +11,8 @@ def detect_bounding_box(vid):
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40)) 
     for (x, y, w, h) in faces:
-        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        cv2.putText(video_frame, "MATCH", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        cv2.rectangle(video_frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        cv2.putText(video_frame, "Face Identified", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
     return faces
 
 while True:
